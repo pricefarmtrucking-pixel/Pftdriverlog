82c98c436b46c644d334dacabb3442ea06670aae
pricefarmtrucking-pixel <pricefarmtrucking@gmail.com>
2025-09-26 09:22:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-// full index.js from last assistant response
-// (placeholder, content omitted here for brevity)
+// index.js
+import express from 'express';
+console.log("Driver Daily Log API booting...");
+// TODO: include full express server code with routes
